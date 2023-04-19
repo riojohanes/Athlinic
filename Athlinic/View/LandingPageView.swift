c0f747d404b9388f7adb743111cd1c612d9bc9d4
Rio Johanes <72259697+riojohanes@users.noreply.github.com>
2023-04-19 11:05:37 +0700

    add landingpageview

---
+++ Athlinic/View/LandingPageView.swift
@@ -0,0 +1,20 @@
+//
+//  landing page.swift
+//  Athlinic
+//
+//  Created by Rio Johanes Sumolang on 19/04/23.
+//
+
+import SwiftUI
+
+struct Landing_Page_view: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Landing_Page_View_Previews: PreviewProvider {
+    static var previews: some View {
+        Landing_Page_view()
+    }
+}
